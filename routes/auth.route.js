a6d5c544171a5440be07ba71d96d44b6c4a7d636
Gayal Melappilly <gayalsunil@gmail.com>
2024-04-29 20:53:52 +0530

    Fixed: Cookie error

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -10,34 +10,36 @@ function isLoggedIn(req, res, next) {
     req.user ? next() : res.sendStatus(401);
 }
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'], prompt: 'select_account' }),(req,res,next)=>{
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'], prompt: 'select_account' }), (req, res, next) => {
     next()
 })
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/auth/failure' }), (req,res)=>{
-    if(!req.user){
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/auth/failure' }), (req, res) => {
+    if (!req.user) {
         console.log("SIGN IN FAILED")
-        res.status(401).json({message: 'Sign in failed'})
+        res.status(401).json({ message: 'Sign in failed' })
     }
     const userData = req.user;
-    const redirectUrl = 'http://localhost:5173/signup';
-    res.redirect(`${redirectUrl}/?userData=${JSON.stringify(userData)}`);
+    // const redirectUrl = 'http://localhost:5173/signup';
 
+    res.cookie('userData', JSON.stringify(userData));
+
+    res.redirect('http://localhost:5173/signup');
 })
 
-router.get('/failure', (req, res)=>{
-    res.status(401).json({message: 'Sign in failed', user: null})
+router.get('/failure', (req, res) => {
+    res.status(401).json({ message: 'Sign in failed', user: null })
 })
 
-router.post('/logout', (req,res)=>{
+router.post('/logout', (req, res) => {
     try {
         console.log("LOG OUT")
-        req.logout((err)=>{
-            if(err){
+        req.logout((err) => {
+            if (err) {
                 console.log(err)
                 res.send('LOGOUT FAILED')
                 return res.status(500).json({ message: 'Failed to logout. QQQ' });
             }
-            console.log("LOG OUT SUCCESS /// USER : ",req.user)
+            console.log("LOG OUT SUCCESS /// USER : ", req.user)
             req.session.destroy((err) => {
                 if (err) {
                     console.error('Error destroying session:', err);
@@ -48,10 +50,10 @@ router.post('/logout', (req,res)=>{
             });
         });
 
-} catch (error) {
+    } catch (error) {
         console.log('Something went wrong.', error.message)
         res.status(500).json({ message: 'Something went wrong.' })
-}
+    }
 })
 
 router.get('/login', login)
