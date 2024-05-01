6b94465df5e7f902b029f6552b13418a4ee7e87a
Gayal Melappilly <gayalsunil@gmail.com>
2024-05-02 02:38:56 +0530

    Friend request updated successfully in DB

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -22,17 +22,44 @@ export const userList = async (req, res) => {
 
 export const addFriend = async (req, res) => {
     try {
-        const senderId = req.body.senderId
-        const receiverId = req.body.receiverId
-        console.log(senderId, receiverId)
+        const senderDetails = req.body.senderDetails
+        const receiverDetails = req.body.receiverDetails
+        console.log(senderDetails, receiverDetails)
 
+        const senderCheck = await UserContactList.findOne({ _id: senderDetails._id })
+        const receiverCheck = await UserContactList.findOne({ _id: receiverDetails._id })
         const ObjectId = mongoose.Types.ObjectId
 
-        User.aggregate([
+        if(senderCheck && receiverCheck) {
+            console.log("USER ALREADY EXISTS, SO UPDATING...")
+            const updatedData = await UserContactList.updateOne(
+                { _id: senderDetails._id },
+                {
+                    $addToSet: {
+                        outgoingRequests: receiverDetails
+                    }
+                }
+            )
+            console.log("UPDATED DATA : ", updatedData)
+
+            const updatedData2 = await UserContactList.updateOne(
+                { _id: receiverDetails._id },
+                {
+                    $addToSet: {
+                        incomingRequests: senderDetails
+                    }
+                }
+            )
+            console.log("UPDATED DATA 2 : ", updatedData2)
+
+            res.status(201).json({msg: 'UPDATED SUCCESSFULLY'})
+        }
+
+        const updatedData = await User.aggregate([
             {
                 $match: {
                     _id: {
-                        $in: [new ObjectId(senderId), new ObjectId(receiverId)]
+                        $in: [new ObjectId(senderDetails._id), new ObjectId(receiverDetails._id)]
                     }
                 }
             },
@@ -43,49 +70,149 @@ export const addFriend = async (req, res) => {
                     contacts: [],
                     incomingRequests: {
                         $cond: {
-                            if: { $eq: ["$_id",new ObjectId(receiverId)] },
-                            then: [{ _id: senderId }],
+                            if: { $eq: ["$_id", new ObjectId(receiverDetails._id)] },
+                            then: [senderDetails],
                             else: []
                         }
                     },
                     outgoingRequests: {
                         $cond: {
-                            if: { $eq: ["$_id",new ObjectId(senderId)] },
-                            then: [{ _id: receiverId }],
+                            if: { $eq: ["$_id", new ObjectId(senderDetails._id)] },
+                            then: [receiverDetails],
                             else: []
                         }
                     }
                 }
-            },
-            {
-                $out: 'user-contact-lists'
             }
-        ]).exec().then((data) => {
-            res.status(201).json(data)
-        }).catch((err) => {
-            res.status(500).json({ message: err.message })
-        });
+        ])
 
+        if(!senderCheck && receiverCheck) {
+            await UserContactList.updateOne(
+                { _id: receiverDetails._id },
+                {
+                    $addToSet: {
+                        incomingRequests: senderDetails
+                    }
+                }
+            )
+
+            await UserContactList.create(
+                {
+                    _id: senderDetails._id,
+                    email: senderDetails.email,
+                    displayName: senderDetails.displayName,
+                    contacts: [],
+                    incomingRequests: [],
+                    outgoingRequests: [receiverDetails]
+                }
+            )
+        }
+
+        if(!receiverCheck && senderCheck){
+            await UserContactList.updateOne(
+                { _id: senderDetails._id },
+                {
+                    $addToSet: {
+                        outgoingRequests: receiverDetails
+                    }
+                }
+            )
+
+            await UserContactList.create(
+                {
+                    _id: receiverDetails._id,
+                    email: receiverDetails.email,
+                    displayName: receiverDetails.displayName,
+                    contacts: [],
+                    incomingRequests: [senderDetails],
+                    outgoingRequests: []
+                }
+            )
+        }
+
+        if(!senderCheck && !receiverCheck) {  
+            await UserContactList.insertMany(updatedData).then((data) => {
+                res.status(201).json(data)
+            }).catch((err) => {
+                console.log("ERROR IN ADD FRIEND : ", err)
+                res.status(500).json({ message: err.message })
+            })
+        }
     }
     catch (error) {
-        console.log("ERROR IN ADD FRIEND : ", error)
+        console.log("ERROR IN ADD FRIEND 2 : ", error)
+        res.status(500).json({ message: error.message })
     }
 }
 
 
-export const requestList = async (req,res) => {
+export const requestList = async (req, res) => {
 
     const userId = req.query.id
 
     try {
-        UserContactList.findOne({_id: userId}).then((data)=>{
+        UserContactList.findOne({ _id: userId }).then((data) => {
             res.status(201).json(data)
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log("ERROR IN REQUEST LIST : ", err)
-            res.status(500).json({message: err.message})
+            res.status(500).json({ message: err.message })
         })
     } catch (error) {
         console.log("ERROR IN REQUEST LIST CATCH : ", error)
-        res.status(500).json({message: err.message})
+        res.status(500).json({ message: err.message })
     }
 }
+
+
+
+
+
+
+
+// const updatedData = await User.aggregate([
+        //     {
+        //         $match: {
+        //             _id: {
+        //                 $in: [new ObjectId(senderDetails._id), new ObjectId(receiverDetails._id)]
+        //             }
+        //         }
+        //     },
+        //     {
+        //         $project: {
+        //           _id: 1,
+        //           email: 1,
+        //           contacts: [],
+        //           incomingRequests: {
+        //             $map: {
+        //               input: {
+        //                 $cond: [
+        //                   { $eq: ["$_id", new ObjectId(receiverDetails._id)] },
+        //                   "$incomingRequests", 
+        //                   []
+        //                 ]
+        //               },
+        //               as: "req",
+        //               in: {
+        //                 $concatArrays: ["$$req", [receiverDetails]] 
+        //               }
+        //             }
+        //           },
+              
+        //           outgoingRequests: {
+        //             $map: {
+        //               input: {
+        //                 $cond: [
+        //                   { $eq: ["$_id", new ObjectId(senderDetails._id)] },  
+        //                   "$outgoingRequests",
+        //                   []
+        //                 ]
+        //               },
+        //               as: "req",  
+        //               in: {
+        //                 $concatArrays: ["$$req", [senderDetails]]
+        //               }
+        //             }
+        //           }
+        //         }
+        //       }
+        // ])
--- models/contact.model.js
+++ models/contact.model.js
@@ -4,12 +4,8 @@ const UserContactListSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     email: String,
     contacts: [],
-    incomingRequests: [{
-        _id: mongoose.Schema.Types.ObjectId
-    }],
-    outgoingRequests: [{
-        _id: mongoose.Schema.Types.ObjectId
-    }]
+    incomingRequests: [],
+    outgoingRequests: []
 },{ timestamps: true });
 
 const UserContactList = mongoose.model('user-contact-list', UserContactListSchema);
